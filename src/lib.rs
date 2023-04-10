6d6bc294d7fcb332fdbc1d2fe43ba1322bd51a62
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-04-10 18:21:13 +0100

    Parse the session info

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,40 @@
-use std::{fs::File, io::{Read, Seek, SeekFrom}};
+mod session_info;
 
-const HEADER_BYTES_SIZE : usize = 112;
-const DISK_HEADER_BYTES_SIZE : usize = 32;
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom},
+    usize,
+};
+
+use session_info::SessionInfo;
+
+const HEADER_BYTES_SIZE: usize = 112;
+const DISK_HEADER_BYTES_SIZE: usize = 32;
 
 pub struct DiskHeader {
     pub start_date: f32,
     pub start_time: f64,
     pub end_time: f64,
     pub lap_count: i32,
-    pub record_count: i32
+    pub record_count: i32,
 }
 
 pub struct Header {
     pub version: i32,
     pub status: i32,
-    pub tick_rate: i32
+    pub tick_rate: i32,
+    pub sesion_info_update: i32,
+    pub sesion_info_offset: i32,
+    pub sesion_info_length: i32,
+    pub num_vars: i32,
+    pub var_header_offset: i32,
+    pub num_buf: i32,
+    pub buf_len: i32,
+    pub buf_offset: i32,
 }
 
 impl From<Vec<u8>> for DiskHeader {
-    fn from(data : Vec<u8>) -> DiskHeader {
+    fn from(data: Vec<u8>) -> DiskHeader {
         DiskHeader {
             start_date: f32::from_le_bytes(data[0..4].try_into().unwrap()),
             start_time: f64::from_le_bytes(data[8..16].try_into().unwrap()),
@@ -30,36 +46,56 @@ impl From<Vec<u8>> for DiskHeader {
 }
 
 impl From<Vec<u8>> for Header {
-    fn from(data : Vec<u8>) -> Header {
-        Header{
+    fn from(data: Vec<u8>) -> Header {
+        Header {
             version: i32::from_le_bytes(data[0..4].try_into().unwrap()),
             status: i32::from_le_bytes(data[4..8].try_into().unwrap()),
             tick_rate: i32::from_le_bytes(data[8..12].try_into().unwrap()),
+            sesion_info_update: i32::from_le_bytes(data[12..16].try_into().unwrap()),
+            sesion_info_length: i32::from_le_bytes(data[16..20].try_into().unwrap()),
+            sesion_info_offset: i32::from_le_bytes(data[20..24].try_into().unwrap()),
+            num_vars: i32::from_le_bytes(data[24..28].try_into().unwrap()),
+            var_header_offset: i32::from_le_bytes(data[28..32].try_into().unwrap()),
+            num_buf: i32::from_le_bytes(data[32..36].try_into().unwrap()),
+            buf_len: i32::from_le_bytes(data[36..40].try_into().unwrap()),
+            buf_offset: i32::from_le_bytes(data[52..56].try_into().unwrap()),
         }
     }
-
 }
 pub struct IbtReader {
     file: File,
     pub header: Header,
-    pub disk_header: DiskHeader
+    pub disk_header: DiskHeader,
+    pub session_info: SessionInfo,
 }
 
 impl IbtReader {
-    pub fn new(file_path : &str) -> IbtReader {
+    pub fn new(file_path: &str) -> IbtReader {
         let mut file = File::open(file_path).expect("Could not load file");
-        let head = read_bytes_file(&mut file, 0, HEADER_BYTES_SIZE);
-        let sub_head = read_bytes_file(&mut file, DISK_HEADER_BYTES_SIZE, HEADER_BYTES_SIZE);
+        let header = Header::from(read_bytes_file(&mut file, 0, HEADER_BYTES_SIZE));
+        let disk_header = DiskHeader::from(read_bytes_file(
+            &mut file,
+            DISK_HEADER_BYTES_SIZE,
+            HEADER_BYTES_SIZE,
+        ));
+        let session_info_data = read_bytes_file(
+            &mut file,
+            header.sesion_info_offset as usize,
+            header.sesion_info_length as usize,
+        );
+        println!("{}", std::str::from_utf8(&session_info_data).unwrap());
         IbtReader {
             file,
-            header: Header::from(head),
-            disk_header: DiskHeader::from(sub_head)
+            header,
+            disk_header,
+            session_info: serde_yaml::from_str(std::str::from_utf8(&session_info_data).unwrap())
+                .unwrap(),
         }
     }
 }
 
-fn read_bytes_file(file : &mut File, from: usize, size: usize) -> Vec<u8>{
-    let mut buffer : Vec<u8> = Vec::with_capacity(size);
+fn read_bytes_file(file: &mut File, from: usize, size: usize) -> Vec<u8> {
+    let mut buffer: Vec<u8> = Vec::with_capacity(size);
     buffer.resize(size, 0);
     file.seek(SeekFrom::Start(from as u64)).unwrap();
     file.read_exact(&mut buffer).unwrap();
@@ -76,12 +112,29 @@ mod tests {
         assert_eq!(reader.header.version, 2);
         assert_eq!(reader.header.tick_rate, 60);
         assert_eq!(reader.header.status, 1);
+        assert_eq!(reader.header.sesion_info_update, 0);
+        assert_eq!(reader.header.sesion_info_offset, 38592);
+        assert_eq!(reader.header.sesion_info_length, 13488);
+        assert_eq!(reader.header.num_vars, 267);
+        assert_eq!(reader.header.var_header_offset, 144);
+        assert_eq!(reader.header.num_buf, 1);
+        assert_eq!(reader.header.buf_len, 1039);
+        assert_eq!(reader.header.buf_offset, 52080);
 
         assert_eq!(reader.disk_header.start_date, 1.401298464324817e-45);
         assert_eq!(reader.disk_header.start_time, 0_f64);
         assert_eq!(reader.disk_header.end_time, 1.105135407938237e-309);
         assert_eq!(reader.disk_header.record_count, 0);
         assert_eq!(reader.disk_header.lap_count, 0);
-    }
 
+        assert_eq!(reader.session_info.weekend_info.track_name, "spielberg gp");
+        assert_eq!(
+            reader
+                .session_info
+                .weekend_info
+                .weekend_options
+                .qualify_scoring,
+            "best lap"
+        );
+    }
 }
---
+++ src/session_info.rs
@@ -0,0 +1,103 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct WeekendInfo {
+    pub track_name: String,
+    #[serde(rename = "TrackID")]
+    pub track_id: i32,
+    pub track_length: String,
+    pub track_length_official: String,
+    pub track_display_name: String,
+    pub track_display_short_name: String,
+    pub track_config_name: String,
+    pub track_city: String,
+    pub track_country: String,
+    pub track_altitude: String,
+    pub track_latitude: String,
+    pub track_longitude: String,
+    pub track_north_offset: String,
+    pub track_num_turns: i32,
+    pub track_pit_speed_limit: String,
+    pub track_type: String,
+    pub track_direction: String,
+    pub track_weather_type: String,
+    pub track_skies: String,
+    pub track_surface_temp: String,
+    pub track_air_temp: String,
+    pub track_air_pressure: String,
+    pub track_wind_vel: String,
+    pub track_wind_dir: String,
+    pub track_relative_humidity: String,
+    pub track_fog_level: String,
+    pub track_cleanup: i32,
+    pub track_dynamic_track: i32,
+    pub track_version: String,
+    #[serde(rename = "SeriesID")]
+    pub series_id: i32,
+    #[serde(rename = "SeasonID")]
+    pub season_id: i32,
+    #[serde(rename = "SessionID")]
+    pub session_id: i32,
+    #[serde(rename = "SubSessionID")]
+    pub sub_session_id: i32,
+    #[serde(rename = "LeagueID")]
+    pub league_id: i32,
+    pub official: i32,
+    pub race_week: i32,
+    pub event_type: String,
+    pub category: String,
+    pub sim_mode: String,
+    pub team_racing: i32,
+    pub min_drivers: i32,
+    pub max_drivers: i32,
+    #[serde(rename = "DCRuleSet")]
+    pub dc_rule_set: String,
+    pub qualifier_must_start_race: i32,
+    pub num_car_classes: i32,
+    pub num_car_types: i32,
+    pub heat_racing: i32,
+    pub build_type: String,
+    pub build_target: String,
+    pub build_version: String,
+    pub weekend_options: WeekendOptions,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct WeekendOptions {
+    pub num_starters: i32,
+    pub starting_grid: String,
+    pub qualify_scoring: String,
+    pub course_cautions: String,
+    pub standing_start: i32,
+    pub short_parade_lap: i32,
+    pub restarts: String,
+    pub weather_type: String,
+    pub skies: String,
+    pub wind_direction: String,
+    pub wind_speed: String,
+    pub weather_temp: String,
+    pub relative_humidity: String,
+    pub fog_level: String,
+    pub time_of_day: String,
+    pub date: String,
+    pub earth_rotation_speedup_factor: i32,
+    pub unofficial: i32,
+    pub commercial_mode: String,
+    pub night_mode: String,
+    pub is_fixed_setup: i32,
+    pub strict_laps_checking: String,
+    pub has_open_registration: i32,
+    pub hardcore_level: i32,
+    pub num_joker_laps: i32,
+    pub incident_limit: String,
+    pub fast_repairs_limit: String,
+    pub green_white_checkered_limit: i32,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct SessionInfo {
+    pub weekend_info: WeekendInfo,
+}
